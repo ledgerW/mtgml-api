1a699a21f5d8efa04e81ac26054b90c16c00856e
Ledger West <ledger.west@gmail.com>
2020-01-07 02:07:23 -0500

    feat: scheduled job for global cards db from scryfall

--- libs/dynamodb_lib.py
+++ libs/dynamodb_lib.py
@@ -5,8 +5,11 @@ from aws_xray_sdk.core import patch
 patch(['boto3'])
 
 def call(table, action, params):
+    client = boto3.client('dynamodb')
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(table)
+
+    print('in dynamodb_lib')
 
     try:
         xray_recorder.begin_subsegment(action)
@@ -21,5 +24,25 @@ def call(table, action, params):
                                           UpdateExpression=params['UpdateExpression'],
                                           ExpressionAttributeValues=params['ExpressionAttributeValues'],
                                           ReturnValues=params['ReturnValues'])
-    except:
+        elif action == 'batch_write_item':
+            return getattr(client, action)(RequestItems=params)
+    except Exception as e:
+        print(e)
         xray_recorder.end_subsegment()
+
+
+def batch_format(val):
+    if type(val) is str:
+        return {"S": str(val)} if len(val)>0 else {"NULL": True}
+    if type(val) is int:
+        return {"N": str(val)}
+    if type(val) is float:
+        return {"N": str(val)}
+    if type(val) is list:
+        return {"SS": val} if len(val)>0 else {"SS": ['empty']}
+    if type(val) is bool:
+        return {"BOOL": True}
+    if val is None:
+        return {"NULL": True}
+    if type(val) is dict:
+        return {"M": {val_key: batch_format(val_val) for val_key, val_val in val.items()}}
---
+++ services/global_data/update_global_cards.py
@@ -0,0 +1,92 @@
+import sys
+sys.path.append('../..')
+
+import os
+import boto3
+import json
+import requests
+import math
+from time import sleep
+import pandas as pd
+import numpy as np
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+import libs.dynamodb_lib as dynamodb_lib
+from libs.response_lib import success, failure
+
+
+CARDS_URL = 'https://api.scryfall.com/cards?page={}'
+CARDS_PER_PAGE = 175
+PAGES_PER_WORKER = 100
+BATCH_LIMIT = 25
+
+client = boto3.client('lambda')
+
+
+def save_scryfall_page(table, page):
+    dynamo_batches = [(first, first+BATCH_LIMIT) for first in list(range(0, CARDS_PER_PAGE, BATCH_LIMIT))]
+
+    res = requests.get(CARDS_URL.format(page))
+    for batch in dynamo_batches:
+
+        cards = res.json()['data'][batch[0]:batch[1]]
+        for idx, card in enumerate(cards):
+            for key, val in card.items():
+                cards[idx][key] = dynamodb_lib.batch_format(val)
+            cards[idx]['cardId'] = cards[idx]['id']
+
+        # Save to Dynamo
+        RequestItems = {
+            table: [
+                {
+                    'PutRequest': {
+                        'Item': card
+                    }
+                } for card in cards
+            ]
+        }
+        _ = dynamodb_lib.call(table, 'batch_write_item', RequestItems)
+
+
+def master(event, context):
+    '''
+    Trigger: Cron event
+    Call Scryfall.com to check number of cards and then distribute
+    # collection to worker function below
+    '''
+    res = requests.get(CARDS_URL.format(1))
+
+    total_cards = res.json()['total_cards']
+    total_pages = math.ceil(total_cards / CARDS_PER_PAGE)
+
+    worker_pages = [(first, first+PAGES_PER_WORKER) for first in list(range(1, total_pages+1, PAGES_PER_WORKER))]
+
+    for pages in worker_pages:
+        response = client.invoke(
+            FunctionName='mtgml-global-data-{}-cards_worker'.format(os.environ['stage']),
+            Payload=json.dumps({"first": pages[0], "last": pages[1]}))
+
+        logger.info(response)
+
+
+def worker(event, context):
+    '''
+    Trigger: http post from master
+    Accept range of scryfall pages to collect from master,
+    then collect them and load to GLOBAL_CARDS_TABLE
+    '''
+    try:
+        table = os.environ['GLOBAL_CARDS_TABLE']
+        pages = json.loads(event['body'])
+
+        # Get cards from Scryfall
+        for page in range(pages['first'], pages['last']):
+            save_scryfall_page(table, page)
+
+        response = success({'status': True})
+    except:
+        response = failure({'status': False})
+
+    return response
